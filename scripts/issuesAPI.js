0585ae25fbff1206483e7dee030ef15f0e91445c
Oishe <yhsiokhan@gmail.com>
2026-03-09 20:58:04 +0600

    Add dynamic data from title, description, id, status and priority

--- scripts/issuesAPI.js
+++ scripts/issuesAPI.js
@@ -14,7 +14,7 @@ const loadOpenIssues = async (status) => {
         const json = await res.json();
 
         openIssues = json.data.filter(issue => issue.status === status);
-       displayOpenCard(openIssues);
+        displayOpenCard(openIssues);
     }
     finally {
         loadingSpinner.classList.add('hidden');
@@ -23,9 +23,37 @@ const loadOpenIssues = async (status) => {
     openIssuesNumber.innerText = openIssues.length;
 }
 // code for display open card
-const displayOpenCard = (cards)=>{
-cards.forEach(card => {
+const displayOpenCard = (cards) => {
+    const allIssues = document.getElementById('all-issues');
+    allIssues.innerHTML = "";
+    cards.forEach(card => {
         console.log(card);
+        const openCards = document.createElement('div');
+        openCards.innerHTML = `
+        <div>
+                <div class="card w-full lg:w-60 bg-base-100 card-sm shadow-sm border border-gray-200">
+                    <div class="card-body">
+                        <div class="flex justify-between items-center">
+                            <div class="flex justify-between">
+                                <div>
+                                    <img class="w-8 h-8"  src="./assets/Open-Status.png" alt="Open-Status">
+                                </div>
+                        </div>
+                            <button class="btn border border-[#EF4444] rounded-lg  bg-[#FEECEC] text-[#EF4444]">High</button>
+                        </div>
+                        <h3 class="text-lg font-semibold">Fix navigation menu on mobile devices</h3>
+                                <p class="gray">The navigation menu doesn't collapse properly on mobile devices...</p>
+                                <div>
+                                    <button class="btn border border-[#EF4444] rounded-lg bg-[#FEECEC] text-[#EF4444] ">Bug</button>
+                                    <button class="btn border border-[#D97706] rounded-lg bg-[#FDE68A] text-[#D97706] ">Help Wanted</button>
+                                </div>
+                                <hr class="border border-gray-200 w-full">
+                                <p class="text-sm gray">#1by john_doe <br> <span class="text-xs gray">1/15/2024</span></p>
+                    </div>
+                </div>
+            </div>
+        `
+        allIssues.append(openCards);
     })
 }
 
@@ -50,9 +78,38 @@ const loadClosedIssues = async (status) => {
     closedIssuesNumber.innerText = closedIssues.length;
 }
 // code for display closed card
-const displayClosedCard = (cards)=>{
-cards.forEach(card => {
+const displayClosedCard = (cards) => {
+    const allIssues = document.getElementById('all-issues');
+    allIssues.innerHTML = "";
+    cards.forEach(card => {
+
         console.log(card);
+        const closedCards = document.createElement('div');
+        closedCards.innerHTML = `
+        <div>
+                <div class="card w-full lg:w-60 bg-base-100 card-sm shadow-sm border border-gray-200">
+                    <div class="card-body">
+                        <div class="flex justify-between items-center">
+                            <div class="flex justify-between">
+                                <div>
+                                    <img class="w-8 h-8"  src="./assets/Open-Status.png" alt="Open-Status">
+                                </div>
+                        </div>
+                            <button class="btn border border-[#EF4444] rounded-lg  bg-[#FEECEC] text-[#EF4444]">High</button>
+                        </div>
+                        <h3 class="text-lg font-semibold">Fix navigation menu on mobile devices</h3>
+                                <p class="gray">The navigation menu doesn't collapse properly on mobile devices...</p>
+                                <div>
+                                    <button class="btn border border-[#EF4444] rounded-lg bg-[#FEECEC] text-[#EF4444] ">Bug</button>
+                                    <button class="btn border border-[#D97706] rounded-lg bg-[#FDE68A] text-[#D97706] ">Help Wanted</button>
+                                </div>
+                                <hr class="border border-gray-200 w-full">
+                                <p class="text-sm gray">#1by john_doe <br> <span class="text-xs gray">1/15/2024</span></p>
+                    </div>
+                </div>
+            </div>
+        `
+        allIssues.append(closedCards);
     })
 }
 
@@ -61,10 +118,46 @@ cards.forEach(card => {
 const displayIssues = (issues) => {
     const allIssuesNumber = document.getElementById('issue-number');
     allIssuesNumber.innerText = issues.length;
+    const allIssues = document.getElementById('all-issues');
+    allIssues.innerHTML = "";
+
     issues.forEach(card => {
         console.log(card);
+
+        
+
+        const allCards = document.createElement('div');
+        allCards.innerHTML = `
+        <div>
+                <div class="card w-full lg:w-60 bg-base-100 card-sm shadow-sm border border-gray-200">
+                    <div class="card-body">
+                        <div class="flex justify-between items-center">
+                            <div class="flex justify-between">
+                                <div>
+                                    ${card.status === 'open'?`<img class="w-8 h-8"  src="./assets/Open-Status.png" alt="Open-Status"></img>`:`<img class="w-8 h-8"  src="./assets/Closed- Status .png"  alt="Closed-Status"></img>`}
+                                </div>
+                        </div>
+                            <button class="btn border rounded-lg ${card.priority === 'medium'?`bg-[#FDE68A] text-[#D97706] border-[#D97706]`:
+                                 card.priority === 'low' ? `bg-[#EEEFF2] text-[#9CA3AF] border-[#9CA3AF]`:`bg-[#FEECEC] text-[#EF4444] border-[#EF4444]`}">
+                                 ${card.priority}
+                            </button>
+                        </div>
+                        <h3 class="text-lg font-semibold">${card.title}</h3>
+                                <p class="gray">${card.description}</p>
+                                <div>
+                                    <button class="btn border border-[#EF4444] rounded-lg bg-[#FEECEC] text-[#EF4444] ">Bug</button>
+                                    <button class="btn border border-[#D97706] rounded-lg bg-[#FDE68A] text-[#D97706] ">Help Wanted</button>
+                                </div>
+                                <hr class="border border-gray-200 w-full">
+                                <p class="text-sm gray">#<span>${card.id}</span> by ${card.author} <br> <span class="text-xs gray">${new Date(card.createdAt).toLocaleDateString()}</span></p>
+                    </div>
+                </div>
+            </div>
+        `
+
+        allIssues.append(allCards);
     })
-    
+
 
     const btnContainer = document.getElementById('btn-container');
     btnContainer.innerHTML = "";
